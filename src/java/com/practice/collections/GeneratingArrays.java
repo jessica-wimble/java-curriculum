ed905565eb7438b12da986fda0b84c4466726420
jessica.wimble <jessica.wimble@autotrader.co.uk>
2025-02-03 09:14:20 +0000

    exercises for arrays

--- src/java/com/practice/collections/GeneratingArrays.java
+++ src/java/com/practice/collections/GeneratingArrays.java
@@ -1,14 +1,20 @@
 package com.practice.collections;
 
+import java.util.List;
+
 /**
- * 1. Initialise a two dimensional integer array, with 3 rows and 5 columns
+ * 1. Initialise a two dimensional integer array (intArray), with 3 rows and 5 columns
  * 2. loop through each entry of the two dimensional array and add a value into every entry
  * 3. access the 2nd element in the 1st row and update it to a new value
+ * 4. access the 2nd element in the intList and print it out.
+ * 5. try remove the last element in the intList - can you explain why this throws an exception?
  */
 public class GeneratingArrays {
 
     public static void main(String[] args){
         Integer[][] intArray = null; // replace me!
+
+        List<Integer> intList = List.of(1,3,4);
 
     }
 
